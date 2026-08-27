1a484122ce9b174b6d427969808d2b6233062402
lammyson <17099142+lammyson@users.noreply.github.com>
2026-08-27 00:47:12 -0400

    Refactor get-room-data. Now has a cache timer and can pass in room and output folder as arguments

---
+++ get-room-data.py
@@ -0,0 +1,113 @@
+import argparse
+from datetime import datetime, timezone
+import json
+from pathlib import Path
+import requests
+
+# Chaos Async 2 "eAI9zbMNRpqFBBAyvCTeaQ"
+# The Archipelago of Questionable Decisions "rc6bMdQiTvCwN7WBN5XOcg"
+# Princess Snivy's Personal Hell: Trigger Happy "E6WqOA-1T8-iemShH8cYyQ"
+
+def download_api_single(
+      api_name: str,
+      endpoint: str,
+      output_folder: str) -> dict:
+   """Download a single file via the Archipelago Web API
+   Args:
+         api_name: Name of the API. Also used for the output filename
+         endpoint: Endpoint to query
+         output_folder: Folder to write the downloaded file to
+   Returns:
+         The downloaded file as a json dict
+   """
+   print(f"Downloading {api_name} to {output_folder}/{api_name}.json")
+   data = requests.get(f"https://archipelago.gg/api/{endpoint}").json()
+   with open(f"{output_folder}/{api_name}.json", "w") as file:
+      json.dump(data, file, indent=3)
+   return data
+
+def download_room_datapackages(
+      api_name: str,
+      static_tracker: dict,
+      output_folder: str,) -> dict:
+   """Download multiple datapackages via the Archipelago Web API
+   Args:
+         api_name: Name of the API. Also used for the output filename
+         static_tracker: dict from the static_tracker endpoint
+         output_folder: Folder to write the downloaded file to
+   Returns:
+         The downloaded file as a json dict
+   """
+   print(f"Downloading {api_name} to {output_folder}/{api_name}.json")
+   data = {
+      game: requests.get(f"https://archipelago.gg/api/datapackage/{static_tracker["datapackage"][game]["checksum"]}").json()
+      for game in static_tracker["datapackage"]
+   }
+   with open(f"{output_folder}/{api_name}.json", "w") as file:
+      json.dump(room_status, file, indent=3)
+   return data
+
+# Parse arguments
+parser = argparse.ArgumentParser(description="Downloads data from an Archipelago room")
+parser.add_argument("-r", "--room-suuid", type=str, required=True, help="Room SUUID. This is a string found in your room's URL. Example: https://archipelago.gg/<ROOM_SUUID>")
+parser.add_argument("-f", "--output-folder", type=str, required=True, help="Output folder. This is where all json files and graphs will be written")
+args = parser.parse_args()
+
+# Create output folder and get the current time
+output_folder = f"{args.output_folder}"
+Path(output_folder).mkdir(parents=True, exist_ok=True)
+now_time = datetime.now(tz=timezone.utc)
+
+# Verify the time the data was last fetched so that we don't request data too quickly
+cache_timeout_s = 300
+if Path(f"{output_folder}/last_fetched.json").exists():
+   with open(f"{output_folder}/last_fetched.json", "r") as s:
+      last_fetched = json.load(s)
+   if "last_fetched" in last_fetched:
+      old_time = datetime.strptime(last_fetched["last_fetched"], "%Y%m%d_%H%M%S").replace(tzinfo=timezone.utc)
+      if (now_time - old_time).seconds <= cache_timeout_s:
+         print(f"Data was last downloaded {(now_time - old_time).seconds} seconds ago which is less than the {cache_timeout_s} second ({cache_timeout_s/60} minute) cache timer. Not downloading room data")
+         exit(0)
+
+print(f"Using room-suuid={args.room_suuid}")
+
+# /room_status/<suuid:room_id> - https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/webhost%20api.md#room_statussuuidroom_id
+# Cache timer: None
+room_status = download_api_single(
+   api_name="room_status",
+   endpoint=f"/room_status/{args.room_suuid}",
+   output_folder=output_folder)
+tracker_suuid = room_status["tracker"]
+
+# /tracker/<suuid:tracker> - https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/webhost%20api.md#trackersuuidtracker
+# Cache timer: 60 seconds
+tracker = download_api_single(
+   api_name="tracker",
+   endpoint=f"/tracker/{tracker_suuid}",
+   output_folder=output_folder)
+
+# /static_tracker/<suuid:tracker> - https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/webhost%20api.md#static_trackersuuidtracker
+# Cache timer: 300 seconds
+static_tracker = download_api_single(
+   api_name="static_tracker",
+   endpoint=f"/static_tracker/{tracker_suuid}",
+   output_folder=output_folder)
+
+# /datapackage/<string:checksum> - https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/webhost%20api.md#datapackagestringchecksum
+# Cache timer: None
+room_datapackages = download_room_datapackages(
+   api_name="room_datapackages",
+   static_tracker=static_tracker,
+   output_folder=output_folder)
+
+# Write the data to file
+with open(f"{output_folder}/room_status.json", "w") as file:
+   json.dump(room_status, file, indent=3)
+with open(f"{output_folder}/tracker.json", "w") as file:
+   json.dump(tracker, file, indent=3)
+with open(f"{output_folder}/static_tracker.json", "w") as file:
+   json.dump(static_tracker, file, indent=3)
+with open(f"{output_folder}/room_datapackages.json", "w") as file:
+   json.dump(room_datapackages, file, indent=3)
+with open(f"{output_folder}/last_fetched.json", "w") as file:
+   json.dump({"last_fetched": datetime.now(tz=timezone.utc).strftime("%Y%m%d_%H%M%S")}, file, indent=3)
--- make-hint-graph.py
+++ make-hint-graph.py
@@ -1,57 +1,25 @@
-#!/usr/bin/env python
+import argparse
 import graphviz
 import json
 
-with open("data/room_status.json", "r") as file:
+parser = argparse.ArgumentParser(description="Make a hint graph a room's data")
+parser.add_argument("-f", "--data-folder", type=str, required=True, help="Folder containing room data retrieved by get-info.py")
+args = parser.parse_args()
+
+# TODO - make a hint graph for a certain slot only and dependent nodes and nodes it depends on
+
+data_folder = args.data_folder
+with open(f"{data_folder}/room_status.json", "r") as file:
    room_status = json.load(file)
-with open("data/tracker.json", "r") as file:
+with open(f"{data_folder}/tracker.json", "r") as file:
    tracker = json.load(file)
-with open("data/static_tracker.json", "r") as file:
+with open(f"{data_folder}/static_tracker.json", "r") as file:
    static_tracker = json.load(file)
-with open("data/room_datapackages.json", "r") as file:
+with open(f"{data_folder}/room_datapackages.json", "r") as file:
    room_datapackages = json.load(file)
 
-# Create a json file with the data required to create the hint graph
-# {
-#    "player": int
-#    "slot_name": str
-#    "alias": str or null
-#    "goal": bool
-#    # Add a hint if found == false and item_flags has 0x1 set
-#    "hints_to_find": { # tracker.json
-#       "receiving_player": int
-#       "finding_player": int
-#       "location": int
-#       "item": int
-#       # "found": bool
-#       # "entrance": str = ""
-#       # "item_flags": int = 0 # 0x1 is progression
-#       # "status": HintStatus = HintStatus.HINT_UNSPECIFIED
-#    }
-# }
-
-# from typing import NamedTuple
-# class Hint(NamedTuple):
-#     receiving_player: int
-#     finding_player: int
-#     location: int
-#     item: int
-#     found: bool
-#     entrance: str = ""
-#     item_flags: int = 0
-#     status: HintStatus = HintStatus.HINT_UNSPECIFIED
-#
-# Example:
-# receiving_player: 1,
-# finding_player: 31,
-# location: 1085039011, # Search the finding_player's locations
-# item: 3626000, # Search the receiving player's items
-# found: false,
-# entrance: "",
-# item_flags: 1,
-# status: 0
-
 # Create an initial list of hint_data
+print("Processing hint data")
 hint_data = [
    {
       "player_num": tracker["aliases"][idx]["player"],
@@ -60,18 +28,36 @@ hint_data = [
       "alias": tracker["aliases"][idx]["alias"],
       "goal": tracker["player_status"][idx]["status"] == 30,
       "hints_to_find": [],
-      "has_hint": False
+      "has_hint": False,
+      "is_item_link": False
    }
    for (idx, slot_name) in enumerate(room_status["players"])
 ]
+
+# Add item_links as their own slot
+for item_link in static_tracker["groups"]:
+   hint_data.append({
+      "player_num": item_link["slot"],
+      "slot_name": item_link["name"],
+      "game": hint_data[item_link["members"][0]-1]["game"],
+      "alias": None,
+      "goal": False,
+      "hints_to_find": [],
+      "has_hint": False,
+      "is_item_link": True
+   })
 
 # Set the pretty node names for later
 for hint_datum in hint_data:
    if hint_datum["alias"]:
       node_name = f"{hint_datum["alias"]} ({hint_datum["slot_name"]})"
+   elif hint_datum["is_item_link"]:
+      node_name = f"item_link: {hint_datum["slot_name"]}"
    else:
       node_name = f"{hint_datum["slot_name"]}"
    hint_datum["node_name"] = node_name
+with open(f"{data_folder}/hint_data_pre.json", "w") as file:
+   json.dump(hint_data, file, indent=3)
 
 # Flatten all the tracker["hints"][idx]["hints"] into a single list while getting rid of dupes
 hints = []
@@ -81,19 +67,14 @@ for hint_dict in tracker["hints"]:
          hints.append(hint)
 
 # Add hints to hint_data
+# https://github.com/ArchipelagoMW/Archipelago/blob/main/docs/network%20protocol.md#hint
 for hint in hints:
    hints_to_find = []
    if hint[4] == False and (hint[6] & 0x1 == 1):
-      # location_names = []
-      # item_names = []
       location = [k for k, v in room_datapackages[hint_data[hint[1]-1]["game"]]["location_name_to_id"].items() if v == hint[2]]
-      # if location:
-      #    location_names.extend(location)
       item = [k for k, v in room_datapackages[hint_data[hint[0]-1]["game"]]["item_name_to_id"].items() if v == hint[3]]
-      # if item:
-      #    item_names.extend(item)
 
-      hint_data[hint[0]-1]["hints_to_find"].append({
+      hint_data[hint[1]-1]["hints_to_find"].append({
          "finding_player": hint[1],
          "receiving_player": hint[0],
          "location_id": hint[2],
@@ -103,36 +84,32 @@ for hint in hints:
       })
       hint_data[hint[0]-1]["has_hint"] = True
       hint_data[hint[1]-1]["has_hint"] = True
-with open("data/hint_data.json", "w") as file:
+with open(f"{data_folder}/hint_data.json", "w") as file:
    json.dump(hint_data, file, indent=3)
 
-# Setup the hint graph and render it
-# save as the following formats: dot, jpg, json, pdf, png, svg
-#
-# Node name
-# if alias == null:
-#    <slot_name>
-# else:
-#    <alias> (<slot_name>) if alias is not
-#
-# Edges are <location_name> <item_name>
-dot = graphviz.Digraph('hint-graph', comment='Hint Graph') 
+# Create the graph
+print("Creating hint graph")
+dot = graphviz.Digraph('hint-graph', graph_attr={'rankdir':'LR'})
 
-# Create all nodes
 for player in hint_data:
    if player["has_hint"]:
       dot.node(f"{player["player_num"]}", player["node_name"])
-
-# Then go make all the edges
-for player in hint_data:
-   for hint in player["hints_to_find"]:
-      dot.edge(tail_name=f"{hint["finding_player"]}",
-               head_name=f"{hint["receiving_player"]}",
-               label=f"{hint["item_name"]} at {hint["location_name"]}")
+      for hint in player["hints_to_find"]:
+         dot.edge(tail_name=f"{hint["finding_player"]}",
+                  head_name=f"{hint["receiving_player"]}",
+                  label=f"{hint["item_name"]} at {hint["location_name"]}")
 
 # Save it!
-engines = ['dot','neato','fdp','sfdp','circo','twopi','osage','patchwork']
+# engines = ['dot','neato','fdp','sfdp','circo','twopi','osage','patchwork']
+engines = ['dot']
+chain = 10
 for engine in engines:
    dot.engine = engine
-   dot.unflatten(chain=10)
-   dot.render(filename=f"{engine}", directory='pics', format='png')
+   dot = dot.unflatten(chain=chain)
+   print(f"Saving to {data_folder}/graphs/{engine}.{chain}.gv.jpg")
+   dot.render(filename=f"{engine}.{chain}.gv", directory=f"{data_folder}/graphs", format='jpg')
+
+for engine in engines:
+   dot.engine = engine
+   print(f"Saving to {data_folder}/graphs/{engine}.gv.jpg")
+   dot.render(filename=f"{engine}.gv", directory=f"{data_folder}/graphs", format='jpg')
